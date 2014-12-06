166382823029691c5d4d4b7e8fab5a079e386067
jenkins <alban.andrieu@free.fr>
2014-12-06 13:06:40 -0600

    Update

--- extract-code.sh
+++ extract-code.sh
@@ -27,6 +27,19 @@ git submodule add https://github.com/AlbanAndrieu/ansible-cmake alban.andrieu.cm
 #git submodule deinit -f alban.andrieu.cpp
 git submodule add https://github.com/AlbanAndrieu/ansible-cpp alban.andrieu.cpp
 
+git submodule add https://github.com/Stouts/Stouts.python.git python
+git submodule add https://github.com/geerlingguy/ansible-role-ntp.git geerlingguy.ntp
+git submodule add https://github.com/AlbanAndrieu/ansible-locale.git locale
+git submodule add https://github.com/AlbanAndrieu/ansible-hostname.git alban.andrieu.hostname
+git submodule add https://github.com/AlbanAndrieu/ansible-java.git java
+git submodule add https://github.com/AlbanAndrieu/ansible-common.git alban.andrieu.common
+git submodule add https://github.com/AlbanAndrieu/ansible-selenium.git alban.andrieu.selenium
+git submodule add https://github.com/AlbanAndrieu/ansible-maven.git maven
+git submodule add https://github.com/AlbanAndrieu/devbox.chrome.git chrome
+git submodule add https://github.com/AlbanAndrieu/ansible-nodejs.git nodejs
+git submodule add https://github.com/AlbanAndrieu/ansible-tomcat.git alban.andrieu.tomcat
+git submodule add https://github.com/AlbanAndrieu/ansible-jboss.git alban.andrieu.jboss
+
 git pull && git submodule init && git submodule update && git submodule status
 #git submodule foreach git pull
 git submodule foreach git checkout master
