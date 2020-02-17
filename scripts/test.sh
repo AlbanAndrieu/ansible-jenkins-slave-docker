af5a5510d11b2f9e6fe0d590720841730056f38e
Andrieu, Alban <alban.andrieu@free.fr>
2020-02-17 23:29:15 +0100

    Ubuntu 19.10

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "ansible-playbook -i inventory/hosts -c local -v playbooks/jenkins-slave-docker.yml --check --diff --ask-become-pass -vvvv"
+ansible-inventory -i inventory/hosts --graph
+
+echo "ansible-playbook -i inventory/hosts -c local playbooks/jenkins-slave-docker.yml -vvvv --check --diff --ask-become-pass"
+echo "ansible-playbook -i inventory/hosts -c local playbooks/jenkins-slave-docker.yml -vvvv --ask-become-pass"
 
 exit 0
