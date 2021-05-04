9dc3bfff78ba23f8c808e0b4a844d7c72f571897
Andrieu, Alban <alban.andrieu@free.fr>
2021-05-04 17:17:46 +0200

    Add ansible-roles-dependencies.py

--- docs/files/ansible-roles-dependencies.py
+++ docs/files/ansible-roles-dependencies.py
@@ -13,6 +13,24 @@ dot = graphviz.Digraph(name='roles')
 role_nodes = {}
 
 
+def normalize_role(role):
+    role = role.replace('/', '_')
+    role = role.replace('.', '_')
+    role = role.replace('-', '_')
+    role = role.replace('geerlingguy_', '')
+    role = role.replace('andrewrothstein_', '')
+    role = role.replace('elliotweiser_', '')
+    role = role.replace('ansiblebit_', '')
+    role = role.replace('jermon_', '')
+    role = role.replace('idealista_', '')
+    role = role.replace('Stouts_', '')
+    role = role.replace('alban_andrieu', 'albanandrieu')
+    role = role.replace('albanandrieu_ansible_common', 'albanandrieu_common')
+    role = role.replace('albanandrieu_', '')
+    role = role.replace('ansible_', '')
+    return role
+
+
 def add_role(role):
     logging.info('Adding role: %s', role)
     role_id = role.replace('/', '_')
@@ -31,6 +49,7 @@ def link_roles(dependent, depended):
 
 for path in glob('roles/*/meta/main.yml'):
     dependent_role = path.split('/')[1]
+    dependent_role = normalize_role(dependent_role)
 
     add_role(dependent_role)
 
@@ -41,6 +60,8 @@ for path in glob('roles/*/meta/main.yml'):
                     depended_role = dependency
                 else:
                     depended_role = dependency.get('role')
+                depended_role = normalize_role(depended_role)
+                logging.info('Adding depended_role: %s', depended_role)
                 if depended_role:
                     add_role(depended_role)
                     link_roles(dependent_role, depended_role)
